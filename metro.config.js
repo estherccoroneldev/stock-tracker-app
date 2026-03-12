d7d11f23d5646ec7e7b8a42ece3ae82a0161a49c
Leomariyer Coronel <lestherccoronel@gmail.com>
2026-03-12 08:40:31 -0300

    sets up expo-notifications package

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,5 @@
 const { getDefaultConfig } = require('expo/metro-config');
-
+const path = require('path');
 /** @type {import('expo/metro-config').MetroConfig} */
 
 const config = getDefaultConfig(__dirname);
@@ -7,5 +7,8 @@ const config = getDefaultConfig(__dirname);
 // pnpm support: ensure metro follows symlinks
 config.resolver.unstable_enableSymlinks = true;
 config.resolver.unstable_enablePackageExports = true;
+
+// Force resolve tamagui to a single location
+config.resolver.nodeModulesPaths = [path.resolve(__dirname, 'node_modules')];
 
 module.exports = config;
